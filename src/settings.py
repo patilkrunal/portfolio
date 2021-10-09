0279cf01bd97ee1b409cf611335c3f93d97a6ef0
patilkrunal <patilkrunal11.kp@gmail.com>
2021-10-09 20:14:34 +0000

    staticfiles added

--- src/settings.py
+++ src/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "secret")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gitkp.pythonanywhere.com']
 
 
 # Application definition
@@ -130,7 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
-
+STATIC_ROOT = "/home/gitkp/krunal_portfolio_backend/static"
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
