48f8f514ff24d2f461080c6d8e9f31d615862a6a
patilkrunal <patilkrunal11.kp@gmail.com>
2021-10-16 22:06:06 +0530

    static media settings changed for development server

--- src/urls.py
+++ src/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from graphene_django.views import GraphQLView
 from api.urls import router as api_router
+from django.conf import settings
+from django.conf.urls.static import static
 from api.schema import schema
 
 urlpatterns = [
@@ -25,3 +27,9 @@ urlpatterns = [
     path('api/', include(api_router.urls)),
     path('api-auth/', include('rest_framework.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, 
+                        document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                        document_root=settings.MEDIA_ROOT)
