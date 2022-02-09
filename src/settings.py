c10534d709e8e1b2cf132a2082a46135b4d3a4e5
patilkrunal <patilkrunal11.kp@gmail.com>
2022-02-09 19:46:00 +0530

    minor fix

--- src/settings.py
+++ src/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY", "secret")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "False")
+DEBUG = os.getenv("DEBUG", "False") == 'True'
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'patilkrunal.herokuapp.com', '*']
 
